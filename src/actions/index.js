0cdd18afbde40aa0f82d1e517782b5fda2dc9aa8
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2018-11-15 23:57:35 -0800

    added axios package and created root url fetchweather function to call service

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,14 @@
 const API_KEY = 'get api key for this app';
+const Root_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export  function fetchWeather(city) {
+const url = `${Root_URL}&q=${city},us`;
+const request = axios.get(url);
+
+  return(
+    type: 'FETCH_WEATHER'
+    payload: request
+  );
+}
