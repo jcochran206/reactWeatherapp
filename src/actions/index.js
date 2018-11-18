adac75d3d0e84905159ad7b9a2fc007279098ea4
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2018-11-18 00:08:40 -0800

    added promise

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'get api key for this app';
+const API_KEY = '03e15524a809381a5969db25b6a5579f';
 const Root_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
@@ -10,7 +10,7 @@ const url = `${Root_URL}&q=${city},us`;
 const request = axios.get(url);
 
   return(
-    type: 'FETCH_WEATHER'
+    type: 'FETCH_WEATHER',
     payload: request
   );
 }
